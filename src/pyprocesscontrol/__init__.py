5d5af7ec81b7e8793bdd634be41ca7bec7f73996
andrewhfaulkner <andrewhfaulkner@hotmail.com>
2023-09-01 12:23:55 -0400

    Initial commit

---
+++ src/pyprocesscontrol/__init__.py
@@ -0,0 +1,7 @@
+from . import controlcharts
+from . import tools
+
+__all__ = ["controlcharts", "tools"]
+__version__ = "0.0.2"
+__author__ = "Andrew Faulkner"
+__license__ = "MIT"
--- src/pyprocesscontrol/tools/__init__.py
+++ src/pyprocesscontrol/tools/__init__.py
@@ -1,9 +1,3 @@
-# Initializing the tools package
-
 from . import datastructures
-from . import KPI
 
-__all__ = ["datastructures", "KPI"]
-__version__ = "0.0.2"
-__author__ = "Andrew Faulkner"
-__license__ = "MIT"
+__all__ = ["tools.datastructures"]
