256051cb6c0fd5b072b688e4cc1e2b2d9122248a
andrewhfaulkner <andrewhfaulkner@hotmail.com>
2023-09-07 16:15:14 -0400

    Adding capability statistics

--- src/pyprocesscontrol/statistics.py
+++ src/pyprocesscontrol/statistics.py
@@ -44,3 +44,67 @@ def shapiro_wilk(data: pd.Series) -> float:
     statsistic, p_val = stats.shapiro(data)
 
     return p_val
+
+
+def cpk(data: pd.Series, lsl: float, usl: float) -> float:
+    """
+    This function calculates the capability statistic for the dataset given a high and low spec limit.
+
+    Args:
+        data: pandas series containing sample data
+        lsl: lower spec limit for the data set
+        usl: upper spec limit for the data set
+
+    Returns:
+        floating point representing the process capability of the system
+    """
+
+    pass
+
+
+def cp(data: pd.Series, lsl: float, usl: float) -> float:
+    """
+    This function calculates the capability statistic for the dataset given a high and low spec limit.
+
+    Args:
+        data: pandas series containing sample data
+        lsl: lower spec limit for the data set
+        usl: upper spec limit for the data set
+
+    Returns:
+        floating point representing the process capability of the system
+    """
+
+    pass
+
+
+def ppk(data: pd.Series, lsl: float, usl: float) -> float:
+    """
+    This function calculates the capability statistic for the dataset given a high and low spec limit.
+
+    Args:
+        data: pandas series containing sample data
+        lsl: lower spec limit for the data set
+        usl: upper spec limit for the data set
+
+    Returns:
+        floating point representing the process capability of the system
+    """
+
+    pass
+
+
+def pp(data: pd.Series, lsl: float, usl: float) -> float:
+    """
+    This function calculates the capability statistic for the dataset given a high and low spec limit.
+
+    Args:
+        data: pandas series containing sample data
+        lsl: lower spec limit for the data set
+        usl: upper spec limit for the data set
+
+    Returns:
+        floating point representing the process capability of the system
+    """
+
+    pass
