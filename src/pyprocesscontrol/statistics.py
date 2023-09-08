a875ce0f315d63c2410ff9c8d77067bc94aa397b
andrewhfaulkner <andrewhfaulkner@hotmail.com>
2023-09-08 09:50:28 -0400

    Bug fixes

--- src/pyprocesscontrol/statistics.py
+++ src/pyprocesscontrol/statistics.py
@@ -7,22 +7,8 @@ import scipy as sp
 from scipy import stats
 
 import pyprocesscontrol as ppc
-
-
-def capability(data: ppc.tools.datastructures) -> float:
-    """
-    The purpose of this function is to calculate the capability statistics of a given dataset/specification
-
-    Args:
-        data: pyprocess control datastructures object containg the data and spec limits for the process
-
-    Returns:
-        floating point number representing the process capability statistic
-    """
-
-    # This function requires some sort of metadata attached to the datastructures object. I will have to add that functionality first.
-
-    pass
+from pyprocesscontrol import tools
+from pyprocesscontrol.tools import datastructures
 
 
 def shapiro_wilk(data: pd.Series) -> float:
--- src/pyprocesscontrol/tools/__init__.py
+++ src/pyprocesscontrol/tools/__init__.py
@@ -1,3 +1,3 @@
 from . import datastructures
 
-__all__ = ["tools.datastructures"]
+__all__ = ["datastructures"]
